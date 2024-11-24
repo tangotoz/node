521c79921516f73c307a635388e44c939f509380
tango <clx050126@outlook.com>
2024-11-24 12:04:50 +0800

    创建 web 服务器

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
-const greeting = 'hello ~';
-console.log(greeting);
+const http = require('http');
+
+const server = http.createServer((req, resp) => {
+    resp.write('hello ~');
+    resp.end();
+});
+
+server.listen(3000, () => {
+    console.log('服务已启动');
+})
