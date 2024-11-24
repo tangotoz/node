2c34caa25dba44629d7a3aa050177800b63728ca
tango <clx050126@outlook.com>
2024-11-24 15:29:35 +0800

    JSON数据传输

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, resp) => {
-    resp.write('hello ~');
+    const data = {
+        id: 1,
+        title: '关山月',
+        content: '明月出天山, 苍茫云海间'
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    resp.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+    resp.write(jsonData);
+
     resp.end();
 });
 
