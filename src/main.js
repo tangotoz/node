8b88291dcd4c08c68477b392942cfb7061f98705
tango <clx050126@outlook.com>
2024-11-24 20:35:57 +0800

    请求头和响应头

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+/**
+ * 使用 JSON 中间件
+ */
+app.use(express.json());
 app.listen(port, () => {
     console.log('服务已启动!');
 });
@@ -41,4 +45,23 @@ app.get('/posts/:postId', (req, resp) => {
     const post = data.filter(item => item.id == postId);
     // 作出响应
     resp.send(post[0]);
+});
+
+/**
+ * 创建内容
+ */
+app.post('/posts', (req, resp) => {
+    // 获取请求里的数据
+    const { content } = req.body;
+
+    // 设置响应头部数据
+    resp.setHeader('Sing-Alone', 'How I wonder what you are');
+
+    // 设置相应状态码
+    resp.status(201);
+
+    // 作出响应
+    resp.send({
+        message: `成功创建了内容: ${content}`
+    });
 });
